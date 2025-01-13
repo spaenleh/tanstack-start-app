85c86b1d4ace81d878e84e627ed302413b75fa40
spaenleh <spaenleh@gmail.com>
2025-01-13 21:20:32 +0100

    fix: setup drizzle and first schemas

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -3,6 +3,13 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const scoreBoardTable = pgTable("scoreboards", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  userId: integer()
+    .notNull()
+    .references(() => usersTable.id),
 });
